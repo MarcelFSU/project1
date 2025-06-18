399e136cda0984ba4c4e30868a9d934bae07a108
MarcelFSU <mschroeder664@gmail.com>
2025-06-18 14:15:23 +0200

    Create map.js

---
+++ map.js
@@ -0,0 +1,16 @@
+const map = L.map('map').setView([50.980, 11.330], 13); // Zentrum Weimar
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© OpenStreetMap-Mitwirkende'
+}).addTo(map);
+
+fetch('punkte.geojson')
+  .then(res => res.json())
+  .then(data => {
+    L.geoJSON(data, {
+      onEachFeature: (feature, layer) => {
+        const name = feature.properties.name || "Ohne Namen";
+        layer.bindPopup(`<strong>${name}</strong>`);
+      }
+    }).addTo(map);
+  });
